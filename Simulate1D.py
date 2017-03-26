5a3f2063ee6b8b4a12108ffdb4d64da0be76dde8
j-curtis <jcurtis2@u.rochester.edu>
2017-03-26 19:08:11 -0400

    Added simulation class to git

---
+++ Simulate1D.py
@@ -0,0 +1,36 @@
+#Jonathan Curtis
+#This is a program for analyzing simulations of the 1-D Ising model
+#PHYS603 Assignment 8 
+
+import numpy as np
+import Metropolis1D as m1d
+
+class Simulation:
+	"""This is a class which runs a specific instance of a simulation of the 1D Ising model"""
+	def __init__(self, Size, J, H, NSteps):
+		"""Initializes and runs a simulation with given lattice size and parameters for given time"""
+		self.latt = m1d.Lattice(Size,J,H)
+		self.nSteps = NSteps
+		
+		#We create variables for observables vs. time
+		#We store the energy, magnetization, and nearest-neighbor correlations (pair) vs. time 
+		#We initialize the arrays to be full of the initial values of these observables
+		self.energy = np.full(shape=self.nSteps,fill_value=self.latt.energyPerSite)	
+		self.mag = np.full(shape=self.nSteps,fill_value=self.latt.magPerSite)		
+		self.pair = np.full(self.nSteps,fill_values=self.latt.pairPerSite)
+	
+	def __repr__(self):
+		return self.pair.__repr__() 
+
+	def simulate(self):
+		"""Runs the simulation and fills the data structures"""
+		for i in np.arange(0,self.nSteps):
+			#Fill observables
+			self.energy[i] = self.latt.energyPerSite
+			self.mag[i] = self.latt.magPerSite
+			self.pair[i] = self.pairPerSite
+
+			#Run one time step
+			self.latt.metropolisStep()
+		
+			
