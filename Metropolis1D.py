f7a1d59209fb4a133a8b8f4f4886a8807afa988e
j-curtis <jcurtis2@u.rochester.edu>
2017-03-26 19:07:50 -0400

    Fixed simulation (was accidentally simulating anti-ferromagnet) and wrote simulation class

--- Metropolis1D.py
+++ Metropolis1D.py
@@ -3,100 +3,82 @@
 #PHYS603 Assignment 8 
 
 import numpy as np 
-import random as rnd
-
-def scaleToSymmInt(x):
-	"""Rescales x in [0,1] to x in [-1,1] symmetrically"""
-	return 2.0*(x-.5)
 
 class Lattice:
 	"""This class is for a 1-D lattice of Ising spins"""
-	def __init__(self, Size,J,H):
-		"""Initializes a lattice of size 'size' with all spins down"""
+	def __init__(self,Size,J,H):
+		"""Initializes a lattice of size 'size' with all spins up and constants J,H"""
 		self.size = Size
 		self.jConst = J		#The lattice spin-spin coupling
 		self.hConst = H		#The lattice magnetization constant
 
 		#We represent the spins as 
-		#down = 0
-		#up = 1
-		self.spins = np.zeros(self.size)
-
-		#Computes the lattice-site average magnetization of the configuation
-		#Initialized to zero, updated when appropriate methods are called 
-		self.magPerSite = 0.0 
+		#down = -1
+		#up = +1
+		#We initialize a new configuation in the 'all-up' configuration
+		self.spins = np.full(shape=self.size,fill_value=1.0)
 		
-		#Computes the lattice-site average pairing interaction of the configuation
-		#Initialized to zero, updated when appropriate methods are called 
-		self.pairPerSite = 0.0
-
-		#Compute the lattice-site average of the total energy of the configuration
-		#Initialized to zero, updated when appropriate methods are called 
-		self.energyPerSite = 0.0
-
-		self.calcEnergy()
+		#We now create variables to store the lattice magnetization, pairing, and energy
+		#These are defined by 
+		#magPerSite = 1/L sum_j S[j]
+		#pairPerSite = 1/L sum_j S[j]S[j+1]
+		#energyPerSite = h*magPerSite + J*pairPerSite
+		#These are all initialized to their value when all the spins are down.
+		#They are updated every metropolis step or when randomized
+		#When all spins are down the magPerSite is 1.0
+		#When all spins are down the pairPerSite is +1.0
+		self.magPerSite = 1.0
+		self.pairPerSite = 1.0
+		self.energyPerSite = -self.hConst - self.jConst
 
 	def __repr__(self):
 		return self.spins.__repr__()
-
-	def randomize(self):
-		"""Randomizes the spins in the lattice with a uniform distribution"""
-		self.spins = np.random.randint(low=0,high=2,size=self.size)
-		self.calcEnergy()
-
+	
 	def calcEnergy(self):
 		"""Computes the lattice-site-average magnetization, pairing, and energy of the lattice"""
 		#Magnetization = 1/N Sum_j S[j]
-		self.magPerSite = scaleToSymmInt( np.float(np.sum(self.spins))/np.float(self.size))
+		self.magPerSite = np.sum(self.spins)/np.float(self.size)
 	
 		#Pairing = 1/N Sum_j S[j] S[j-1]
-		pairInt = 0.0
-		for i in np.arange(0,self.size):
-			pairInt += scaleToSymmInt(self.spins[i])*scaleToSymmInt(self.spins[i-1])
+		self.pairPerSite = np.sum(self.spins[:]*self.spins[:-1])/np.float(self.size)
 
-		pairInt /= np.float(self.size)
+		#Energy = -h*mag - J*pairing
+		self.energyPerSite = -self.hConst*self.magPerSite - self.jConst*self.pairPerSite
 
-		self.pairPerSite = pairInt 
+	def randomize(self):
+		"""Randomizes the spins in the lattice with a uniform distribution"""
+		self.spins = np.random.randint(np.array([-1.0,1.0]),size=self.size)
+		self.calcEnergy()
 
-		#Energy = h*mag + J*pairing
-		self.energyPerSite = self.hConst*self.magPerSite + self.jConst*self.pairPerSite
-
-
-	def metropolis(self):
+	def metropolisStep(self):
 		"""This method implements the metropolis algorithm for flipping a spin"""
 		"""First it randomly selects a site on the lattice"""
 		"""Then it flips the spin with a probability related to the difference in energies"""
 		
 		#First we pick a random site
 		#Then we retrieve its value and the value of its neighbors
-		site = np.random.randint(0,self.size)
-		spin = self.spins[site]
-		
 		#They are put into an array as [site-1, site, site+1]
-		neighbors = np.array([self.spins[site-1],spin,self.spins[(site+1)%(self.size)]])	
 		#Note the mod % for the implementation of periodic boundary conditions
+		site = np.random.randint(0,self.size)
+		neighbors = np.array([self.spins[site-1],self.spins[site],self.spins[(site+1)%(self.size)]])	
 		
-		#We rescale all spins to the appropriate range
-		neighbors[:]= scaleToSymmInt(neighbors[:])
-
 		#Now we calculate the change in energy we would get by flipping this spin 
 		#We also compute the change in magnetization and pairing 
-		#For a single spin-1/2 Ising spin being flipped the change in energy for flipping S[j] can be computed as 
 		pairChange = -2.0*neighbors[1]*(neighbors[0]+neighbors[2])
 		magChange = -2.0*neighbors[1]
-		energyChange = self.hConst*magChange + self.jConst*pairChange	
+		energyChange = -self.hConst*magChange - self.jConst*pairChange	
 
 		#Now we accept the spin flip with the probability of min(1,e^{-Delta E})
 		#To do this, we draw a number r randomly from Uniform(0,1)
-		#We accept the flip if r < e^{-DeltaE}
-		#That way, if the energy is lowered $DeltaE<0$ and this will always be accepted 
-		#If the energy is raised, it will be accepted with probability $e^{-Delta E}<1$.
-		testnum = np.random.ranf()
-
+		#We accept the flip if r < exp(-energyChange)
+		#That way, if the energy is lowered energyChange<0 and this will always be accepted since exp(-energyChange) >=1
+		#If the energy is raised, it will be accepted with probability 0<exp(-energyChange)<1
 		#If we accept the change, we will also update the magnetization and energy of the lattice by adding the changes 
 		#It also returns whether the flip was accepted or rejected 
+		testnum = np.random.ranf()
+
 		if testnum < np.exp(-energyChange):
-			self.spins[site] = 1 - spin	#This flips the spin value 
+			self.spins[site] *= -1.0	#This flips the spin value 
 			
 			#Now we update the internal variables
 			self.energyPerSite += energyChange/float(self.size)
@@ -108,20 +90,7 @@ class Lattice:
 			return 0
 
 def main():
-	lattice = Lattice(10,1.0,1.0)
-	lattice.randomize()
-	print(lattice)
-
-	lattice.calcEnergy()
-	print(lattice.magPerSite)
-	print(lattice.pairPerSite)
-	print(lattice.energyPerSite)
-
-	lattice.metropolis()
-	print(lattice.magPerSite)
-	print(lattice.pairPerSite)
-	print(lattice.energyPerSite)
-	
+	print("No Test Code")	
 
 if __name__ == "__main__":
 	main()
