0efdd3d3398a031c806fc6a49a2283ce82606459
j-curtis <jcurtis2@u.rochester.edu>
2017-04-01 22:39:09 -0400

    Merged simulation of 1D and 2D lattices into one class

--- Simulate.py
+++ Simulate.py
@@ -1,15 +1,22 @@
 #Jonathan Curtis
-#This is a program for analyzing simulations of the 1-D Ising model
+#This is a program for analyzing simulations of the Ising model
 #PHYS603 Assignment 8 
 
 import numpy as np
 import Metropolis1D as m1d
+import Metropolis2D as m2d
 
 class Simulation:
-	"""This is a class which runs a specific instance of a simulation of the 1D Ising model"""
-	def __init__(self, Size, J, H, NSteps):
+	"""This is a class which runs a specific instance of a simulation of the 1D or 2D Ising model"""
+	def __init__(self, Dimension, Size, J, H, NSteps):
 		"""Initializes and runs a simulation with given lattice size and parameters for given time"""
-		self.latt = m1d.Lattice(Size,J,H)
+		self.dim = Dimension
+
+		if self.dim == 2:
+			self.latt = m2d.Lattice(Size,J,H)
+		else:
+			self.latt = m1d.Lattice(Size,J,H)
+
 		self.nSteps = NSteps
 		
 		#We create variables for observables vs. time
